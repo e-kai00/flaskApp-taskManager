647e909c23a6d83b29e5114735510895c60c2df0
Katerina <yelkaterina@gmail.com>
2023-05-25 11:48:00 +0200

    Add functionality to display categories to the user

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,7 +10,9 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    # categories(used in categories.html)=categories(function variable)
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
